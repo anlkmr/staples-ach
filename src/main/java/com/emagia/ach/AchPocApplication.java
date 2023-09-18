7da31510bb13591601c8d1aee27002d543bf250e
anlkmr <anil.kadiga@emagia.com>
2023-09-18 08:24:13 +0530

    init

---
+++ src/main/java/com/emagia/ach/AchPocApplication.java
@@ -0,0 +1,13 @@
+package com.emagia.ach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emagia/ach/AchPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emagia.ach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
